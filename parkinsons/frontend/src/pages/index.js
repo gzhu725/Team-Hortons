d2a0fdf6a8eef93a1a9a7fac5b7ea9a1c58cb597
gzhu725 <gloriazhu66@gmail.com>
2025-02-08 13:11:43 -0500

    setup 'home' page

---
+++ parkinsons/frontend/src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Typography, Button, AppBar, Toolbar } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My MUI React Page</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" style={{ textAlign: "center", marginTop: "2rem" }}>
+        <Typography variant="h4" gutterBottom>
+          Welcome to My Page
+        </Typography>
+        <Typography variant="body1" paragraph>
+          This is a simple React page using Material-UI components.
+        </Typography>
+        <Button variant="contained" color="primary">
+          Click Me
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
