8032e14fb1923ea81a839c55d13678b806438a97
gzhu725 <gloriazhu66@gmail.com>
2025-02-08 16:10:11 -0500

    more frontend home additions

--- parkinsons/frontend/src/pages/index.js
+++ parkinsons/frontend/src/pages/index.js
@@ -1,27 +1,82 @@
-import React from "react";
-import { Container, Typography, Button, AppBar, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+import { Container, AppBar, Box } from "@mui/material";
+import Navbar from "../components/Navbar";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Home = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
   return (
     <>
       <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">My MUI React Page</Typography>
-        </Toolbar>
+        <Navbar />
       </AppBar>
       <Container
         maxWidth="md"
         style={{ textAlign: "center", marginTop: "2rem" }}
       >
-        <Typography variant="h4" gutterBottom>
-          Welcome to My Page
-        </Typography>
-        <Typography variant="body1" paragraph>
-          This is a simple React page using Material-UI components.
-        </Typography>
-        <Button variant="contained" color="primary">
-          Click Me
-        </Button>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="flex-start"
+          style={{ width: "100%" }}
+        >
+          <Box
+            style={{
+              flex: 1, // Takes up 1/3 of the space
+              maxWidth: "33%", // Ensure it doesn't exceed a third of the container's width
+              padding: "1rem",
+            }}
+          >
+            <Calendar
+              onChange={handleDateChange}
+              value={date}
+              style={{
+                width: "100%", // Fill width
+                height: "100%", // Fill height
+              }}
+            />
+          </Box>
+          <Box
+            style={{
+              flex: 2, // Right section 2/3
+              maxWidth: "66%",
+              padding: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "80vh", // Same height as left section
+            }}
+          >
+            {/* Top Section (inside the right section) */}
+            <Box
+              style={{
+                flex: 1, // Takes up part of the vertical space
+                marginBottom: "1rem",
+                border: "1px solid #ccc",
+                padding: "1rem",
+              }}
+            >
+              daily symptom how do u feel and typed notes
+            </Box>
+
+            {/* Bottom Section (inside the right section) */}
+            <Box
+              style={{
+                flex: 1, // Takes up part of the vertical space
+                border: "1px solid #ccc",
+                padding: "1rem",
+              }}
+            >
+              more data
+            </Box>
+          </Box>
+        </Box>
       </Container>
     </>
   );
